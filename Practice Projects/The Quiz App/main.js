6e97307ff3fd808b048c364b158ea6f95a508d0a
Jaydeep Vejapara <jaydeeppatel8150@gmail.com>
2024-08-30 18:18:31 +0530

    Quiz App final

--- Practice Projects/The Quiz App/main.js
+++ Practice Projects/The Quiz App/main.js
@@ -6,8 +6,10 @@ $(document).ready(function () {
       // console.log(data[2].options[1]);
 
       $(".container").html(`<div id="q-container"></div>`);
-      $(".container").append(`<div class="submit"><button id="submit">Submit</button></div>`);
-      
+      $(".container").append(
+        `<div class="submit"><input type=submit id="submit"></div>`
+      );
+
       for (let i = 0; i < data.length; i++) {
         $("#q-container").append(
           `<div class=q` +
@@ -23,26 +25,51 @@ $(document).ready(function () {
         // Append options to div
         $("#q-container").children("div").addClass("que");
         for (let j = 0; j < data[i].options.length; j++) {
-          $(`.que:eq(${i})`).append(`<label for="ans_${j}_${data[i].options[j].toString().replace(/ /g, "_")}"><input type="radio" name="question_${i}" id="ans_${j}_${data[i].options[j].toString().replace(/ /g, "_")}" />${data[i].options[j].toString()}</label>`);
+          $(`.que:eq(${i})`).append(
+            `<label for="ans_${j}_${data[i].options[j]
+              .toString()
+              .replace(
+                / /g,
+                "_"
+              )}"><input type="radio" name="question_${i}" id="ans_${j}_${data[
+              i
+            ].options[j]
+              .toString()
+              .replace(/ /g, "_")}" />${data[i].options[j].toString()}</label>`
+          );
         }
 
+        // add eventlistner on submit
+        // $("#submit").on("click", function () {
+        //   let score = 0;
+        //   for (let k = 0; k < data.length; k++) {
+        //     let qAnswerNo = data[i].answer;
+        //     let fAns = data[i].options[qAnswerNo];
+        //     let userAns = $(
+        //       `.que:eq(${i}) input[type=radio][name="question_${i}"]:checked`
+        //     ).val();
+        //   }
+        // });
 
-       
+        $("#submit").on("click", function () {
+          let score = 0;
+          for (let k = 0; k < data.length; k++) {
+            let qAnswerNo = data[k].answer; // index of ans
+            let fAns = data[k].options[qAnswerNo]; //converting that index value to actual ans
+            let userAns = $(
+              `input[type=radio][name="question_${k}"]:checked`
+            ).attr("id");
+            if (
+              userAns ===
+              `ans_${qAnswerNo}_${fAns.toString().replace(/ /g, "_")}`
+            ) {
+              score++;
+            }
+          }
 
-
-      // add eventlistner on submit
-      $("#submit").on("click", function () {
-        let score = 0;
-        for (let k = 0; k < data.length; k++) {
-          let qAnswerNo = data[i].answer;
-          let fAns = data[i].options[qAnswerNo]
-          let userAns = $(`.que:eq(${i}) input[type=radio][name="question_${i}"]:checked`).val()
-         }
-      })
+          $(".result:last").children("p").children("span").text(`${score}`);
+        });
       }
-
-      
-      
     }
   ).fail(function () {
     alert("Try after sometime");
